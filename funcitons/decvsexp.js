c598c5bc3582adb134e9b088eb4e2a605f37eff7
Clint <clinthiles1992@gmail.com>
2024-10-13 09:31:30 -0700

    Add function declarations and expressions for calculating age

---
+++ funcitons/decvsexp.js
@@ -0,0 +1,14 @@
+// 1. Function Declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// 2. Function Expression
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age1, age2);
