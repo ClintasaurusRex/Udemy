9c1f52aa2cc9a0a7681d4d3ccd272ea2b2a75d88
Clint <clinthiles1992@gmail.com>
2024-10-14 18:21:48 -0700

    Refactor code to loop backwards through an array

---
+++ loops/loopBackwards.js
@@ -0,0 +1,7 @@
+// Looping backwards
+
+const clint = ["Clint", "Arneson-Hiles", 2024 - 1992, "developer", ["Michael", "Steven", "Jay"]];
+
+for (let i = clint.length - 1; i >= 0; i--) {
+  console.log(i, clint[i]);
+}
