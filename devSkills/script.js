fc5d68c08b45657d819d4905bb2855aa2b812078
Clint <clinthiles1992@gmail.com>
2024-10-15 17:19:39 -0700

    Refactor code to remove debugging comments and add a function to print forecasted temperatures

--- devSkills/script.js
+++ devSkills/script.js
@@ -1,1 +1,91 @@
 "use strict";
+
+// Debugging
+
+// const measureKelvin = function () {
+//   const measurement = {
+//     typeof: "temp",
+//     unit: "celsius",
+
+//     // C) FIX THE BUG
+//     // value: Number(prompt("Degrees celsius:")),
+//   };
+
+//   // console.log(measurement);
+//   // B) FIND THE BUG
+//   console.table(measurement);
+
+//   // console.log(measurement.value);10
+
+//   // console.warn(measurement.value);
+//   // console.error(measurement.value);
+
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+// // A) IDENTIFY BUG
+// console.log(measureKelvin());
+
+// // Using a debugger
+// const calcTempAmplitudeBug = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+
+//   let max = 0;
+//   let min = 0;
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+
+// // A) IDENTIFY BUG
+// console.log(amplitudeBug);
+
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+// 1) Understanding the problem
+// Array transformed into a string seperated by ...
+// What is the X days? current index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into a string
+// - Transform each element to string with ºC
+// - String needs to contain day (index + 1)
+// - Add ... between elements at start and end of string
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+// console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log("...", str);
+};
+
+printForecast(data1);
