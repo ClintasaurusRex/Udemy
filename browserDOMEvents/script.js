7dc1aa1f39418d7830d465465610d3e962fda818
Clint <clinthiles1992@gmail.com>
2024-10-22 15:21:32 -0700

    Implemented score functionality

--- browserDOMEvents/script.js
+++ browserDOMEvents/script.js
@@ -23,11 +23,38 @@ console.log(document.querySelector('.guess').value);
 // Event Listener: A function that performs an action based on a certain event. It waits for a specific event to happen
 // The first argument is the event that the function is waiting for and the second argument is the function that will be called when the event happens (callback function)
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'Pick a number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    score++;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'YOU LOST THE GAME';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'YOU LOST THE GAME';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
