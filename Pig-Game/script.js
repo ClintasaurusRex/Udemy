90c198972328a1f3e8850794a25c3dd8820b6e3d
Clint <clinthiles1992@gmail.com>
2024-10-25 15:17:22 -0700

    Refactor code to add dice rolling functionality, improve player switching logic, and implement hold button functionality

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -27,6 +27,14 @@ let playing = true;
 score0El.textContent = 0;
 score1El.textContent = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // Rolling functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -44,10 +52,24 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // Switch player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  console.log('Hold button');
+  // 1. Add current score to active player
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // 2. Check if players score is>= 100
+  if (scores[activePlayer] >= 100) {
+    // Finnish the games
+    document
+      .querySelector(`player--${activePlayer}`)
+      .classList.add('player--winner');
+  }
+
+  // 3. Switch to next player
+  switchPlayer();
 });
