fe916d8824d4c112f860e88b2f6f749d7053b48a
Clint <clinthiles1992@gmail.com>
2024-12-18 08:32:58 -0800

    The for of loop

--- dataStructures, operators, strings/script.js
+++ dataStructures, operators, strings/script.js
@@ -71,8 +71,32 @@ const restaurant = {
   },
 };
 
-/////////////////////////// Logical assignment operator
+//////////////////////////// The for of loop
 
+// The for of loops are used to iterate over arrays, strings, maps, sets, and other iterable objects.
+// It works by iterating over the values of an iterable object and executing a block of code for each value.
+// your can use break and continue statements in for of loops
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+// this is the old way
+for (const item of menu.entries()) {
+  console.log(`${item[0] + 1}: ${item[1]}`);
+}
+console.log('--------Old way above----New below');
+// New way
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+// the entries method returns an array of arrays, each array is a key-value pair
+// the first value is the index and the second value is the value
+
+// console.log([...menu.entries()]);
+
+/////////////////////////// Logical assignment operator
+/*
 const rest1 = {
   name: 'Capri',
   // numGuests: 20,
@@ -104,6 +128,8 @@ rest2.owner &&= '<ANONYMOUS>'; // {name: 'La Piazza', owner: '<ANONYMOUS>', numG
 
 console.log(rest1);
 console.log(rest2);
+*/
+
 /////////////////////////////////// Nullish Coalescing operator
 /*
 // The way this works is that it only works with null and undefined values so if the value is 0 or '' it will still return the value
