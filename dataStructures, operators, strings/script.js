2ac9ee6fa2be62cb20f1b4cec747e65c17357ff5
Clint <clinthiles1992@gmail.com>
2024-12-17 16:31:33 -0800

    Nullish operator

--- dataStructures, operators, strings/script.js
+++ dataStructures, operators, strings/script.js
@@ -71,6 +71,52 @@ const restaurant = {
   },
 };
 
+/////////////////////////// Logical assignment operator
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0, // 0 is falsy so we know from the console that ||= is working
+};
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// Or assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// This is the same as above but better
+// OR assignment operator
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>'; // {name: 'Capri', numGuests: 0}
+rest2.owner &&= '<ANONYMOUS>'; // {name: 'La Piazza', owner: '<ANONYMOUS>', numGuests: 10}
+
+console.log(rest1);
+console.log(rest2);
+/////////////////////////////////// Nullish Coalescing operator
+/*
+// The way this works is that it only works with null and undefined values so if the value is 0 or '' it will still return the value
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+// Nullish: null and undefined (NOT 0 or ')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+*/
+/*
 /////////////////////// Short circuiting (&& and ||)
 console.log('------------OR------------------');
 
@@ -102,7 +148,7 @@ if (restaurant.orderPizza) {
 }
 // This does the same as the if block
 restaurant.orderPizza && restaurant.orderPizza('Meaty', 'Onions');
-
+*/
 ////////////////////////////// Rest pattern and Parameters
 /*
 // SPREAD, because on the right side of =
