bed1013b3e344c53fe204fa17a0a58190180cf38
Clint <clinthiles1992@gmail.com>
2024-12-18 14:46:10 -0800

    New set operations

--- dataStructures, operators, strings/script.js
+++ dataStructures, operators, strings/script.js
@@ -21,24 +21,6 @@ const openingHours = {
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-const italianFoods = new Set([
-  'pasta',
-  'gnocchi',
-  'tomatoes',
-  'olive oil',
-  'garlic',
-  'basil',
-]);
-
-const mexicanFoods = new Set([
-  'tortillas',
-  'beans',
-  'rice',
-  'tomatoes',
-  'avocado',
-  'garlic',
-]);
-
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -99,6 +81,52 @@ const restaurant = {
 
 /////////////////////////// SETS
 
+////New ops with SETS
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
+
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
+
+// intersection finds common elements in side and array
+const commonFoods = italianFoods.intersection(mexicanFoods);
+console.log('Intersection:------ ', commonFoods);
+
+const foodsArr = [...commonFoods];
+for (const [i, common] of [...foodsArr].entries()) {
+  console.log(`${i + 1}: ${common}`);
+}
+
+const italianMexicanFusion = italianFoods.union(mexicanFoods); // merges the two arrays
+console.log('Union:------ ', italianMexicanFusion);
+
+console.log([new Set([...italianFoods, ...mexicanFoods])]); // merged arrays without duplicates
+
+const uniqueItalianFoods = italianFoods.difference(mexicanFoods); // Shows the difference between arrays
+console.log('Difference italian ', uniqueItalianFoods);
+
+const uniqueMexicanFoods = mexicanFoods.difference(italianFoods); // Shows the difference between arrays
+console.log('Difference Mexican ', uniqueMexicanFoods);
+
+const uniqueItalianAndMexicanFoods =
+  italianFoods.symmetricDifference(mexicanFoods); // This will give both unique elements in each array
+console.log(uniqueItalianAndMexicanFoods);
+
+console.log(italianFoods.isDisjointFrom(mexicanFoods)); // this will check if the array has no elements in common
+
+/*
 const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pizza']);
 
 console.log(ordersSet);
@@ -129,7 +157,7 @@ console.log(new Set('Clint Arneson-Hiles').size);
 // for (const [i, set] of [...ordersSet].entries()) {
 //   console.log(`${i + 1}: ${set}`);
 // }
-
+*/
 /////////////////////////////// Looping Objects: Object Keys, Values and Entries
 /*
 // Property names: Object Keys loops only the keys of the object
