8fc8106924ae53cdcf2cbef40977a836fe5fde6d
Clint <clinthiles1992@gmail.com>
2024-12-19 14:18:39 -0800

    Default Values

--- closer-look-at-functions/script.js
+++ closer-look-at-functions/script.js
@@ -1,8 +1,37 @@
 'use strict';
 
-document.querySelector('.buy').addEventListener('click', function () {
-  console.log('Buy new Plane button clicked');
-});
-document.querySelector('.poll').addEventListener('click', function () {
-  console.log('Poll button clicked');
-});
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numOfPassengers = 1,
+  price = 199 * numOfPassengers
+) {
+  // numOfPassengers = numOfPassengers || 1; ES5 way, ugly
+  // price = price | 199;
+
+  const booking = {
+    flightNum,
+    numOfPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000); // {flightNum: 'LH123', numOfPassengers: 1, price: 1000}
+/*
+for (const booking of bookings) {
+  // console.log(booking.flightNum, booking.numOfPassengers, booking.price);
+  console.log(
+    `Flight:${booking.flightNum} has ${booking.numOfPassengers} passengers. $${booking.price}`
+  );
+}
+for (const [key, value] of Object.entries(bookings[0])) {
+  console.log(key, ':', value);
+}
+*/
