9f4d7378ab68c7ebe32c2adb0711a800e71d219e
Clint <clinthiles1992@gmail.com>
2024-12-19 14:34:53 -0800

    Passing arguments

--- closer-look-at-functions/script.js
+++ closer-look-at-functions/script.js
@@ -1,5 +1,36 @@
 'use strict';
 
+///////////////////////// How passing arguments wors: Value vs Reference
+
+const flight = 'LH234';
+const clint = {
+  name: 'Clint Arneson-Hiles',
+  passport: 3930293945,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'Lh999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 3930293945) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+// checkIn(flight, clint);
+// console.log(flight);
+// console.log(clint);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(clint);
+checkIn(flight, clint);
+
+///////////////////////////// Default Values
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -24,6 +55,7 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000); // {flightNum: 'LH123', numOfPassengers: 1, price: 1000}
+*/
 /*
 for (const booking of bookings) {
   // console.log(booking.flightNum, booking.numOfPassengers, booking.price);
