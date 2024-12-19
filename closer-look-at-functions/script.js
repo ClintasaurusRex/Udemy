a822da0dbb1138541ae24cb8e34ce79b112a32bb
Clint <clinthiles1992@gmail.com>
2024-12-19 13:41:46 -0800

    Started a closer look at functions module

---
+++ closer-look-at-functions/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+document.querySelector('.buy').addEventListener('click', function () {
+  console.log('Buy new Plane button clicked');
+});
+document.querySelector('.poll').addEventListener('click', function () {
+  console.log('Poll button clicked');
+});
