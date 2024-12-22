cd1d8c5dd4619486bdcc82d2ba77b731dc988b2e
Clint <clinthiles1992@gmail.com>
2024-12-22 13:32:29 -0800

    Started IFFE

--- closer-look-at-functions/script.js
+++ closer-look-at-functions/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/////////////////////////////////// Immediately invoked function expressions (IFFE)
+// an IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined.
+// It is a design pattern which is also known as a Self-Executing Anonymous Function and contains two major parts:
+// The first is the anonymous function with lexical scope enclosed within the Grouping Operator (). This prevents accessing variables within the IIFE idiom as well as polluting the global scope. The second part creates the immediately invoked function expression () through which the JavaScript engine will directly interpret the function.
+
 /*
 //////////////////////////////// The call and apply methods
 const lufthanasa = {
