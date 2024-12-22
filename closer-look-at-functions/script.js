ab42ba239eb1fbd6604559fccbf1e7ac2df473f6
Clint <clinthiles1992@gmail.com>
2024-12-22 13:43:46 -0800

    IFFE done

--- closer-look-at-functions/script.js
+++ closer-look-at-functions/script.js
@@ -5,6 +5,27 @@
 // It is a design pattern which is also known as a Self-Executing Anonymous Function and contains two major parts:
 // The first is the anonymous function with lexical scope enclosed within the Grouping Operator (). This prevents accessing variables within the IIFE idiom as well as polluting the global scope. The second part creates the immediately invoked function expression () through which the JavaScript engine will directly interpret the function.
 
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+// IFFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+console.log(notPrivate);
+console.log(isPrivate);
 /*
 //////////////////////////////// The call and apply methods
 const lufthanasa = {
