739fd155e3715c14907119aa029f823d941780e7
Clint <clinthiles1992@gmail.com>
2024-12-22 11:16:29 -0800

    challenge finished

--- closer-look-at-functions/script.js
+++ closer-look-at-functions/script.js
@@ -36,8 +36,39 @@ const poll = {
   // This generates [0,0,0,0] Moring the the next section
 
   answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')} (Write a option number)`
+      )
+    );
+    console.log(answer);
+    // Register the answer
+
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    // console.log(this.answers);
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll Results are ${this.answers.join(', ')}`);
+    }
+  },
 };
 
+// poll.registerNewAnswer();
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 /*
 //////////////////////////////// The call and apply methods
 const lufthanasa = {
