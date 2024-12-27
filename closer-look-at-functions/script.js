c48b7848d98d9db1a9921522efc037c89d72f66f
Clint <clinthiles1992@gmail.com>
2024-12-27 07:38:24 -0800

    coding challenge

--- closer-look-at-functions/script.js
+++ closer-look-at-functions/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red'
+
+
+
 ///////////////////////////////// Closures ///////////////////////////////////////
 
 // a closure is the closed-over variable environment of the execution context in which a function was created, even after that execution context is gone
@@ -24,6 +42,7 @@ booker();
 console.dir(booker);
 */
 
+/*
 ///////////////////// More closures
 // Example 1
 let f;
@@ -65,7 +84,7 @@ const boardPassengers = function (n, wait) {
 
 const perGroup = 1000;
 boardPassengers(180, 3);
-
+*/
 /*
 /////////////////////////////////// Immediately invoked function expressions (IFFE)
 // an IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined.
