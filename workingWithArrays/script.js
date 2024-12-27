1048e3ca1d5399d13aa733b726158fd97fb70d3a
Clint <clinthiles1992@gmail.com>
2024-12-27 11:03:20 -0800

    refactor: comment out array initialization and enhance explanations for slice and reverse methods

--- workingWithArrays/script.js
+++ workingWithArrays/script.js
@@ -75,7 +75,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 // let arr = ['a', 'b', 'c', 'd', 'e'];
-
+/*
 // SLICE
 console.log(arr.slice(2)); // ['c', 'd', 'e']
 console.log(arr.slice(2, 4)); // ['c', 'd'] the 4 tells js where to stop
@@ -90,6 +90,12 @@ console.log([...arr]); // ['a', 'b', 'c', 'd', 'e']
 arr.splice(-1);
 arr.splice(1, 2); // The second argument is how many elements we want to delete
 console.log(arr); // ['a', 'b', 'c', 'd']
-
-// REVERSE
+*/
+// REVERSE --  This mutates the original array as well
 let arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+
+console.log(arr2.reverse()); // ['f', 'g', 'h', 'i', 'j']
+console.log(arr2);
+
+// CONCAT
