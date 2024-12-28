5b7f81931da9090e5b71d579e20864e96e917c2d
Clint <clinthiles1992@gmail.com>
2024-12-28 13:43:50 -0800

    Chaining methods

--- workingWithArrays/script.js
+++ workingWithArrays/script.js
@@ -423,3 +423,14 @@ const eurToUsd = 1.1;
       console.log(arr1, arr2);
       
       */
+/////////////////// Chaining Methods /////////////////////////////////////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD); // 5522.000000000001
