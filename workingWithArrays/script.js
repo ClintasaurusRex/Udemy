637f34ad20789428660052750cb3bdbd293d438c
Clint <clinthiles1992@gmail.com>
2024-12-29 09:47:54 -0800

    refactor: reorganize comments for find method and improve clarity

--- workingWithArrays/script.js
+++ workingWithArrays/script.js
@@ -474,18 +474,19 @@ const movmentDescriptions = movements.map((mov, i, arr) => {
       const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
       console.log(avg1, avg2);
       
-      */
-
-//////////////// Find Method ///////////////////////
-
-// Used to retrieve one element of an array based on an condition
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-// returns first element that satisfies the condition
-const firstWithdrawl = movements.find(mov => mov < 0);
-console.log(movements);
-console.log(firstWithdrawl);
-
-console.log(accounts);
-
-let account = accounts.find(acc => (acc.owner = 'Jessica Davis'));
+      
+      //////////////// Find Method ///////////////////////
+      
+      // Used to retrieve one element of an array based on an condition
+      const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+      
+      // returns first element that satisfies the condition
+      const firstWithdrawl = movements.find(mov => mov < 0);
+      console.log(movements);
+      console.log(firstWithdrawl);
+      
+      console.log(accounts);
+      
+      let account = accounts.find(acc => (acc.owner = 'Jessica Davis'));
+      
+*/
