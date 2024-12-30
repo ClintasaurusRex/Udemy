db729cbaf5e167a5792fbc49fbf1544b154acb4d
Clint <clinthiles1992@gmail.com>
2024-12-30 15:41:20 -0800

    feat: add documentation and examples for flat and flatMap methods in array movements

--- workingWithArrays/script.js
+++ workingWithArrays/script.js
@@ -627,7 +627,7 @@ const movmentDescriptions = movements.map((mov, i, arr) => {
         } movements ago`
       ); // Your lastest large movement was 7 movements ago
       
-*/
+
 ///////////////////////////////////////////////////////////////////////////////////
 // some AND every METHOD
 
@@ -654,3 +654,39 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit)); // true
 console.log(movements.every(deposit)); // false
 console.log(movements.filter(deposit)); //  [200, 450, 3000, 70, 1300]
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Flat and FlatMap ***********************
+
+// The flat method is used to flatten an array of arrays into a single array. It is used to flatten an array of arrays into a single array.
+
+// The flatMap method is used to flatten an array of arrays into a single array. It is used to flatten an array of arrays into a single array.
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+
+console.log(arr.flat()); // [1, 2, 3, 4, 5, 6, 7, 8]
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2)); // The 2 in this map tells js to go 2 levels deep into the array
+
+const accountMovements = accounts.map(acc => acc.movements);
+console.log(accountMovements);
+// const allMovements = accountMovements.flat();
+// console.log(allMovements);
+// const overallBalance = allMovements.reduce((acc, cur) => acc + cur, 0);
+
+// flat Method
+const overallBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, cur) => acc + cur, 0);
+console.log(overallBalance);
+
+// flatMap method
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements) // Same result as about - flatMap can only go one level deep
+  .reduce((acc, cur) => acc + cur, 0);
+console.log(overallBalance);
