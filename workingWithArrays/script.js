3c7b781809b93b2a858377a53f9cd22d9e0b4a7f
Clint <clinthiles1992@gmail.com>
2024-12-30 09:58:52 -0800

    fix: update transfer button logic to find receiver account by username

--- workingWithArrays/script.js
+++ workingWithArrays/script.js
@@ -181,7 +181,11 @@ btnLogin.addEventListener('click', function (event) {
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
-  const receiverAcc = inputTransferTo.value;
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+
+  console.log(amount, receiverAcc);
 });
 
 /////////////////////////////////////////////////
