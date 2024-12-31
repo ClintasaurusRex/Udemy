5e01274898c2555fb4317815cdbf318c7ac9f2e9
Clint <clinthiles1992@gmail.com>
2024-12-31 12:51:13 -0800

    feat: enhance displayMovements function to support optional sorting of movements

--- workingWithArrays/script.js
+++ workingWithArrays/script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (movement, index) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (movement, index) {
     const type = movement > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -259,6 +261,12 @@ btnClose.addEventListener('click', function (e) {
 
   inputCloseUsername.value = inputClosePin.value = '';
   inputLoginPin.blur();
+});
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  displayMovements(currentAccount.movements, true);
 });
 
 /////////////////////////////////////////////////
