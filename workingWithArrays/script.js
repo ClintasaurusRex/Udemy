592d73c1c682416b54740af114f49f40015fdf09
Clint <clinthiles1992@gmail.com>
2024-12-31 14:22:02 -0800

    feat: add examples for array fill and Array.from methods in script.js

--- workingWithArrays/script.js
+++ workingWithArrays/script.js
@@ -829,7 +829,7 @@ console.log(sorted); //[-650, -400, -130, 70, 200, 450, 1300, 3000]
 // Descending order
 
 // console.log(movements.sort((a, b) => b - a)); // [3000, 1300, 450, 200, 70, -130, -400, -650]
-*/
+
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -860,3 +860,44 @@ console.log(groupedByActivity); // {very active: Array(3), active: Array(1)}
 // const groupedByType = Object.groupBy(accounts, acc => acc.type);
 const groupedByType = Object.groupBy(accounts, ({ type }) => type); // with destructing
 console.log(groupedByType); // {premium: Array(2), standard: Array(1), basic: Array(1)}
+
+*/
+//////////////////////////////////////////////////////////////////////
+/// More ways of creating and filling arrays
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+
+// x.fill(1);
+x.fill(1, 3, 5); // [empty × 3, 1, 1, empty × 2]
+console.log(x);
+
+arr.fill(23, 4, 6);
+console.log(arr); // [1, 2, 3, 4, 23, 23, 7] mutates original array
+
+// Array.from()
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y); // [1, 1, 1, 1, 1, 1, 1]
+
+const z = Array.from({ length: 7 }, (cur, i) => i + 1);
+console.log(z); // [1, 2, 3, 4, 5, 6, 7] use _ where cur goes as its a throw away parameter
+
+const randomDiceRoll = Array.from(
+  { length: 10 },
+  cur => Math.trunc(Math.random(cur) * 20) + 1
+);
+console.log(randomDiceRoll); // [5, 5, 5, 1, 2, 6, 6, 5, 1, 3]
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI); // [1300, 70, -130, -650, 3000, -400, 450, 200]
+
+  // const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+  // console.log(movementsUI2);
+});
