153dbfae05806514f76f29057685f527537d4977
Clint <clinthiles1992@gmail.com>
2025-01-02 09:50:26 -0800

    feat: update script.js with examples for toReversed and toSorted methods, and add usage of with method

--- workingWithArrays/script.js
+++ workingWithArrays/script.js
@@ -911,13 +911,17 @@ console.log(numbers); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // Non- Destructive alternatives toReversed, toSorted, toSpliced
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-console.log(movements);
+console.log(movements); // [200, 450, -400, 3000, -650, -130, 70, 1300]
 
 // reversed method is destructive so if a slice method is in front of it the original will be untouched
 
 // toReversed method
 const reversedMovements = movements.toReversed();
 console.log(reversedMovements);
-console.log(movements);
 
-// toSorted Mehtod
+// toSorted Mehtod (sort), toSpliced (splice)
+
+// movements[1] = 2000;
+const newMovements = movements.with(1, 2000);
+console.log(newMovements);
+// console.log(movements); // [200, 2000, -400, 3000, -650, -130, 70, 1300]
