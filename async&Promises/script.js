07622bccaf6c3653eb8f4d9b8dc1ed9e9b0c2ac7
Clint <clinthiles1992@gmail.com>
2025-01-07 08:52:08 -0800

    feat: add additional test cases for 'whereAmI' function with new GPS coordinates

--- async&Promises/script.js
+++ async&Promises/script.js
@@ -218,3 +218,5 @@ const whereAmI = function (lat, lng) {
     .catch(err => console.error(`${err.message} 💥`));
 };
 whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
